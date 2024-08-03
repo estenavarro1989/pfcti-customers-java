376d11f5a0ec523df68ce84520942e39ff4e4b6a
Alejandro Navarro <estenavarro@gmail.com>
2024-08-02 18:00:14 -0600

    fix: remove unused imposrts

--- src/main/java/com/pfcti/customers/model/Customer.java
+++ src/main/java/com/pfcti/customers/model/Customer.java
@@ -3,15 +3,10 @@ package com.pfcti.customers.model;
 import java.time.LocalDate;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.NamedStoredProcedureQuery;
-import jakarta.persistence.ParameterMode;
-import jakarta.persistence.StoredProcedureParameter;
 import lombok.Data;
 
 @Data
 @Entity
-@NamedStoredProcedureQuery(name = "get_customers_order_by_birth_date_desc", procedureName = "get_customers_order_by_birth_date_desc", resultClasses = Customer.class, parameters = {
-        @StoredProcedureParameter(mode = ParameterMode.REF_CURSOR, name = "p_result", type = Customer.class) })
 public class Customer {
 
     @Id
